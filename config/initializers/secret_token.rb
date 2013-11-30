930f29548819efb46db41b65633e5c2f3374dc6d
Ted Coleman <ted.coleman@sendgrid.com>
2013-11-29 19:59:42 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JennysCloset::Application.config.secret_key_base = '2750b1529519a3a72ee2cb3603f2ec8b24f38c50df2dd400d446d1c4d4de2c15961512aea0d1f67ae62820390426634d8411d37a55b9aeaee9241a0e0d945489'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JennysCloset::Application.config.session_store :cookie_store, key: '_jennys_closet_session'
